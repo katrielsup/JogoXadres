462713c9d96174161b630d6dce41476369f5c021
Katriel <katrielsp046@gmail.com.br>
2019-08-11 13:52:34 -0300

    Versao 1.1

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -7,7 +7,7 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		Posicao posicao = new Posicao(3, 5);
-		System.out.println(posicao);
+		System.out.println(posicao);a
 	}
 
 }
