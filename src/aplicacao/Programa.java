afcb26fe59e67b64a622fa12e49c05ab08913414
Katriel <katrielsp046@gmail.com.br>
2019-08-11 13:51:55 -0300

    Versao 1.1

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,13 @@
 package aplicacao;
 
+import jogodetabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-
+		Posicao posicao = new Posicao(3, 5);
+		System.out.println(posicao);
 	}
 
 }
